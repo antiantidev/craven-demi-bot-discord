25961cd631ff77f0c4c88de4e3eafeecb5b67c54
Nhat Linh Nguyen <antiantidev@gmail.com>
2025-04-29 20:52:55 +0700

    Fix token to ENV

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config(); // <- Load biến môi trường từ .env
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
 const mongoose = require("mongoose");
 const { deployCommands } = require("./utils/deployCommands");
 
@@ -33,7 +32,7 @@ for (const folder of commandFolders) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
       );
     }
   }
@@ -80,4 +79,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 
 // === LOGIN BOT ===
-client.login(token);
+client.login(process.env.TOKEN);
